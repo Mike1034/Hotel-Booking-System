290d9982c64c8d52f4d944830fb11842f5266f35
Mike1034 <linmyat10324@gmail.com>
2024-11-06 08:32:03 +0000

    first commit

---
+++ components/user_header.php
@@ -0,0 +1,22 @@
+<!-- header section starts  -->
+
+<section class="header">
+
+   <div class="flex">
+      <a href="#home" class="logo">Royal Lake View</a>
+      <a href="#availability" class="btn">check availability</a>
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </div>
+
+   <nav class="navbar">
+      <a href="home.php#home">Home</a>
+      <a href="home.php#about">About</a>
+      <a href="home.php#reservation">Reservation</a>
+      <a href="home.php#gallery">Gallery</a>
+      <a href="home.php#contact">Contact</a>
+      <a href="bookings.php">Bookings</a>
+   </nav>
+
+</section>
+
+<!-- header section ends -->
